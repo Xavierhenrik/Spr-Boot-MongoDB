9cda16e245b8e3c5fe06d7b8b3a68adbdbcd684b
Xavierhenrik <dev.henriquexavier@gmail.com>
2025-10-23 12:35:03 -0300

    Entity user and Restworking

---
+++ src/main/java/com/xavier/sprbootmongodb/resources/UserResource.java
@@ -0,0 +1,31 @@
+package com.xavier.sprbootmongodb.resources;
+
+import com.xavier.sprbootmongodb.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll() {
+
+        User maria = new User("1001", "Maria Brown", "maria@gmail.com");
+        User alex = new User("1002", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(maria, alex));
+
+        return ResponseEntity.ok().body(list);
+
+    }
+
+
+}
