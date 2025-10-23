d5cbc6b6dfda8f774742c7257db2109204b52a96
Xavierhenrik <dev.henriquexavier@gmail.com>
2025-10-23 11:33:43 -0300

    Initialize Spring Boot project with MongoDB

---
+++ src/test/java/com/xavier/sprbootmongodb/SprBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.sprbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprBootMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
