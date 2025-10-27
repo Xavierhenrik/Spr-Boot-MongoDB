538acb844e2739d7454b24c5aabceb480a92a90d
Henrique Xavier <henrique.xavier@maida.health>
2025-10-27 11:06:51 -0300

    user insertion with POST

--- src/main/java/com/xavier/sprbootmongodb/services/UserService.java
+++ src/main/java/com/xavier/sprbootmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.xavier.sprbootmongodb.services;
 
 import com.xavier.sprbootmongodb.domain.User;
+import com.xavier.sprbootmongodb.dto.UserDTO;
 import com.xavier.sprbootmongodb.repository.UserRepository;
 import com.xavier.sprbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +23,20 @@ public class UserService {
     }
 
     public User findById(String id) {
-        
+
         User user = repo.findById(id).orElse(null);
 
         if (user == null){
             throw new ObjectNotFoundException("User not found");
         }
         return user;
+    }
+
+    public User insert(User user) {
+        return repo.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(null, userDTO.getName(), userDTO.getEmail());
     }
 }
